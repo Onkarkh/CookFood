f4a0435551c98c81d433434e0df3371afe7f7985
onkar <khaladkaronkar@gmail.com>
2019-10-21 03:07:22 +0530

    Back Button added

--- app/src/main/java/phonetism/cookmyfood/MainActivity.java
+++ app/src/main/java/phonetism/cookmyfood/MainActivity.java
@@ -1,8 +1,10 @@
 package phonetism.cookmyfood;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.widget.Toast;
 
@@ -14,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         loadMainMenu();
         toastMessage("Long Press the Recipe to Save!");
+
     }
     private void toastMessage(String message) {
         Toast.makeText(this, "" + message, Toast.LENGTH_LONG).show();
@@ -23,5 +26,31 @@ public class MainActivity extends AppCompatActivity {
         FragmentManager fragmentManager = getSupportFragmentManager();
         RecipeMenu recipeMenu = new RecipeMenu();
         fragmentManager.beginTransaction().replace(R.id.mainFragment, recipeMenu).commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Select one of the Following");
+        builder.setCancelable(true);
+
+        builder.setNegativeButton(
+                "Cancel",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+
+        builder.setPositiveButton(
+                "Quit",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        System.exit(0);
+                    }
+                });
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 }
